0e18949a9d2221172c9bfe74b2ac1535c594cb0f
Eduardo <eduardotakeuchi@outlook.com>
2022-06-10 13:56:54 -0300

    Biding

--- p-angular/src/app/app.module.ts
+++ p-angular/src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { C1Component } from './c1.component';
-import { C2Component } from './c2/c2.component';
+
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    C1Component,
-    C2Component
+    CounterComponent
   ],
   imports: [
     BrowserModule,
--- p-angular/src/app/counter/counter.component.spec.ts
+++ p-angular/src/app/counter/counter.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { C2Component } from './c2.component';
+import { CounterComponent } from './counter.component';
 
-describe('C2Component', () => {
-  let component: C2Component;
-  let fixture: ComponentFixture<C2Component>;
+describe('CounterComponent', () => {
+  let component: CounterComponent;
+  let fixture: ComponentFixture<CounterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ C2Component ]
+      declarations: [ CounterComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(C2Component);
+    fixture = TestBed.createComponent(CounterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
